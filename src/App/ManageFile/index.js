1ec13bd7d4195a4d1beb6f8a90aeea1b4a1a5d0c
Tejas <kubeekubee59@gmail.com>
2022-01-30 17:29:48 +0530

    update: Integrated roles and file details in the fileView page

--- src/App/ManageFile/index.js
+++ src/App/ManageFile/index.js
@@ -1,59 +1,108 @@
 import React, { Component } from "react";
-import axios from "axios"
+import axios from "axios";
 import { reactLocalStorage } from "reactjs-localstorage";
-import { Buffer } from 'buffer';
-import { useLocation, useNavigate, useParams } from 'react-router-dom';
+import { Buffer } from "buffer";
+import { useLocation, useNavigate, useParams } from "react-router-dom";
 
 class ManageFile extends Component {
-
   constructor(props) {
-    super(props)
-    this.state = { isFileLoaded: false }
+    super(props);
+    this.state = { isFileLoaded: false };
   }
 
   componentDidMount() {
-    console.log(this.props.location)
-    let fileId = this.getFileIdFromUrl()
-    let authToken = reactLocalStorage.get('authToken');
-    const config = { headers: { Authorization: `Bearer ${authToken}` }, responseType: 'arraybuffer' };
-    this.setState({
-      fileId: fileId,
-      config: config
-    }, () => {
-      this.getFile()
-    })
+    console.log(this.props.location);
+    let fileId = this.getFileIdFromUrl();
+    let authToken = reactLocalStorage.get("authToken");
+    const arrayBufferConfig = {
+      headers: { Authorization: `Bearer ${authToken}` },
+      responseType: "arraybuffer",
+    };
+    const config = {
+      headers: { Authorization: `Bearer ${authToken}` },
+    };
+    this.setState(
+      {
+        fileId: fileId,
+        arrayBufferConfig: arrayBufferConfig,
+        config: config,
+      },
+      () => {
+        this.getFile();
+        this.getFileDetails();
+        this.getRoleOfFile();
+      }
+    );
   }
+
   getFile = () => {
-    axios.get(`/file/${this.state.fileId}`, this.state.config).then((response) => {
-      console.log(response)
-      console.log(response.headers["content-type"])
-      let base64ImageString = Buffer.from(response.data, 'binary').toString('base64')
-      let srcValue = `data:${response.headers["content-type"]};base64,` + base64ImageString
+    axios
+      .get(`/file/${this.state.fileId}`, this.state.arrayBufferConfig)
+      .then((response) => {
+        console.log(response);
+        console.log(response.headers["content-type"]);
+        let base64ImageString = Buffer.from(response.data, "binary").toString(
+          "base64"
+        );
+        let srcValue =
+          `data:${response.headers["content-type"]};base64,` +
+          base64ImageString;
 
-      this.setState({ fileData: srcValue, isFileLoaded: true, fileName: response.headers["content-disposition"], contentType: response.headers["content-type"] });
+        this.setState({
+          fileData: srcValue,
+          isFileLoaded: true,
+          fileName: response.headers["content-disposition"],
+          contentType: response.headers["content-type"],
+        });
+      })
+      .catch((err) => {
+        console.log(err);
+        // alert(err.status + err.response)
+      });
+  };
 
+  getFileDetails = () => {
+    axios
+      .get(`/file/${this.state.fileId}/details`, this.state.config)
+      .then((response) => {
+        console.log(response);
+        this.setState({
+          fileDetails: response.data,
+        });
+      })
+      .catch((err) => {
+        console.log(err);
+        // alert(err.status + err.response)
+      });
+  };
 
-    }).catch((err) => {
-      console.log(err)
-      // alert(err.status + err.response)
-    });
-  }
+  getRoleOfFile = () => {
+    axios
+      .get(`/file/${this.state.fileId}/role`, this.state.config)
+      .then((response) => {
+        console.log(response);
+        this.setState({
+          role: response.data.role,
+        });
+      })
+      .catch((err) => {
+        console.log(err);
+        // alert(err.status + err.response)
+      });
+  };
 
   getFileIdFromUrl = () => {
-    return this.props.location["fileId"]
-  }
-
+    return this.props.location["fileId"];
+  };
 
   download = () => {
     let a = document.createElement("a");
     a.href = this.state.fileData;
     a.download = this.state.fileName;
     a.click();
-  }
-
+  };
 
   render() {
-
     // return (<div>
     //   {this.state.isFileLoaded ?
     //     this.state.contentType.includes("image") ?
@@ -73,32 +122,57 @@ class ManageFile extends Component {
     //   }
 
     // </div>);
+    return (
+      <div>
+        {this.state.isFileLoaded ? (
+          <div>
+            <h2>{this.state.fileDetails.filename}</h2>
+            <div style={{ display: "flex" }}>
+              <label>Role: </label> <span>{this.state.role}</span>
+            </div>
+          </div>
+        ) : null}
+        {this.state.isFileLoaded && this.state.contentType.includes("image") ? (
+          <div>
+            <img
+              style={{
+                display: "block",
+                marginLeft: "auto",
+                marginRight: "auto",
+                width: "50%",
+                maxWidth: "1000px",
+                maxHeight: "500px",
+              }}
+              src={this.state.fileData}
+              alt=""
+            ></img>
+          </div>
+        ) : this.state.isFileLoaded ? (
+          <div>
+            <p>
+              Unable To display the file. File type: {this.state.contentType}
+            </p>
+          </div>
+        ) : (
+          <div>Loading please wait.. </div>
+        )}
 
-    if (this.state.isFileLoaded && this.state.contentType.includes("image")) {
-      return (
-        <div>
-          <img style={{ display: "block", marginLeft: "auto", marginRight: "auto", width: "50%", maxWidth: "1000px", maxHeight: "500px" }}
-            src={this.state.fileData} alt=""></img>
-          <button onClick={() => { this.download() }}>Download</button>
-        </div>
-      );
-    }
-    else if (this.state.isFileLoaded) {
-      return (
-        <div>
-          <p>Unable To display the file. File type: {this.state.contentType}</p>
-          <button onClick={() => { this.download() }}>Download</button>
-        </div>
-      )
-    }
-    else {
-      return (<div>Loading please wait.. </div>)
-    }
+        {this.state.isFileLoaded ? (
+          <button
+            onClick={() => {
+              this.download();
+            }}
+          >
+            Download
+          </button>
+        ) : null}
+      </div>
+    );
   }
 }
 export default (props) => {
-  const history = useNavigate()
-  const location = useParams()
+  const history = useNavigate();
+  const location = useParams();
 
-  return <ManageFile history={history} location={location} {...props} />
-} 
+  return <ManageFile history={history} location={location} {...props} />;
+};
